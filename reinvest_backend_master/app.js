79e9daaa6ef8e0d7fc3b31b2ed40a6b24f655963
Jacky Chen <jacky.chen.6@gmail.com>
2020-12-25 17:08:48 -0500

    cors test

--- reinvest_backend_master/app.js
+++ reinvest_backend_master/app.js
@@ -18,7 +18,7 @@ app.use('/',express.static(__dirname));
 app.use(cookies());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 mongoose
   .connect(uri, {
@@ -33,7 +33,28 @@ mongoose
     console.log("Error " + err);
   });
 
+
+
 app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 Routes(app);
 
 app.listen(port, () => {
