9e54ad70df1ef06aa55e7b73c0fcc25f068850eb
Jacky Chen <jacky.chen.6@gmail.com>
2020-12-27 14:18:40 -0500

    secured all links

---
+++ reinvest_frontend/src/loading.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import {Ring} from 'react-awesome-spinners';
+
+export const loadingPage = () =>{
+  return (
+    <div style={{
+      position: 'absolute', left: '50%', top: '50%',
+      transform: 'translate(-50%, -50%)'
+  }}>
+      <Ring size = {120}/>
+    </div>
+  )
+}
