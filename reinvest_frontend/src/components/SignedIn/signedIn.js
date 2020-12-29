211aaacbddcdb4f6482005e905c7a06f5307e768
Horace <horaceliu.hl@gmail.com>
2020-12-29 14:42:32 -0500

    search bar update

--- reinvest_frontend/src/components/SignedIn/signedIn.js
+++ reinvest_frontend/src/components/SignedIn/signedIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import { properties } from "./properties";
 import Property from "./Property";
@@ -8,14 +8,27 @@ import Navb from "../Signedout/Navbar"; //importing from navbar.js?
 import SearchBar from "./SearchBar"
 
 function PropertyList(props) {
+  const [keyword, setKeyword] = useState();
+
+  const handleKeyword = () => {
+
+  }
   return (
     <>
       <Navb auth ={props.auth}/>
+      <div class="centerSearchBar">
+        <input 
+        class="barStyling"
+        key="random1"
+        value={keyword}
+        placeholder={"search property"}
+        onChange={handleKeyword}
+        />
+      </div>
       <section className="booklist">
         {properties.map((property) => {
           return <Property key={property.id} {...property}></Property>;
         })}
-        <SearchBar></SearchBar>
         <Link to="/propertyinfo">Add New Property</Link>
       </section>
     </>
